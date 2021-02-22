9bc23de1be3441cb03c50f88ab59b0edb246fc37
Juliano Marcos Martins <juliano.jmm@gmail.com>
2021-02-22 17:54:41 -0300

    Adicionei opcoes e usando Intelij

--- jogoNaruto.go
+++ jogoNaruto.go
@@ -7,19 +7,23 @@ import (
 func main() {
 	var opcao int
 	var pontos int = 0
-        fmt.Println("Onde nasceu o Naruto?");
-	fmt.Println("1) Aldeia da Folha");
-	fmt.Println("2) Candido Mota");
-	fmt.Println("3) Assis");
-	fmt.Scanln(&opcao);
+    
+
+    fmt.Println("Onde nasceu o Naruto?")
+	fmt.Println("1) Aldeia da Folha")
+	fmt.Println("2) Aldeia da Nevoa")
+	fmt.Println("3) Aldeia da Pedra")
+	fmt.Println("4) Aldeia do Som")
+	fmt.Scanln(&opcao)
  
 	if opcao == 1 {
-		fmt.Println("Voce acertou!!!");	
+		fmt.Println("Voce acertou!!!")
 		pontos = 1
 	} else {
-		fmt.Println("Voce errou!");
+		fmt.Println("Voce errou!")
+		fmt.Println("Voce é um BURRO! Dá ZERO pra ele!!!!!")
 	}
 
- 	fmt.Println("Fim do Jogo!");
+ 	fmt.Println("Fim do Jogo!")
 	fmt.Printf("Voce fez %v pontos", pontos)
 }
